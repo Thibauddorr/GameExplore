d5a4cae21e521ce79f25e4025a290249710a87ac
thibaud dorr <thibaud.dorr@gmail.com>
2023-09-13 09:55:24 -0400

    Initial Commit

---
+++ GameExplore/GameExploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameExploreApp.swift
+//  GameExplore
+//
+//  Created by Thibaud DORR on 13/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct GameExploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
