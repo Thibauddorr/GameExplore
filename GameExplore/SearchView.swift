9e52f661c6709b536812f26a295612ea00eff642
thibaud dorr <thibaud.dorr@gmail.com>
2023-09-14 10:52:00 -0400

    Create SearchBar in Search view

--- GameExplore/SearchView.swift
+++ GameExplore/SearchView.swift
@@ -8,13 +8,73 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var searchText = ""
+    
     var body: some View {
-        Text("Search View")
+        VStack{
+            SearchBar(text: $searchText)
+            Spacer()
+                
+        }
+        .padding()
     }
 }
 
 struct SearchView_Previews: PreviewProvider {
     static var previews: some View {
         SearchView()
+    }
+}
+
+struct SearchBar: View {
+    @Binding var text: String
+    @State private var isEditing = false // Add this state property
+
+    var body: some View {
+        HStack {
+            TextField("Search", text: $text, onEditingChanged: { editing in
+                withAnimation {
+                    self.isEditing = editing
+                }
+            })
+            .padding(7)
+            .padding(.horizontal, 25)
+            .background(Color(.systemGray6))
+            .cornerRadius(8)
+            .overlay(
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                        .foregroundColor(.gray)
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 8)
+
+                    if !text.isEmpty {
+                        Button(action: {
+                            withAnimation {
+                                self.text = ""
+                            }
+                        }) {
+                            Image(systemName: "multiply.circle.fill")
+                                .foregroundColor(.gray)
+                                .padding(.trailing, 8)
+                        }
+                    }
+                }
+            )
+            .padding(.horizontal, 10)
+            
+            if isEditing {
+                Button(action: {
+                    withAnimation {
+                        self.text = ""
+                        self.isEditing = false
+                    }
+                }) {
+                    Text("Cancel")
+                }
+                .padding(.trailing, 10)
+                .transition(.move(edge: .trailing))
+            }
+        }
     }
 }
